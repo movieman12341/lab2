25d260b9925448c776bf60091e97e3f4de6438f5
movieman12341 <movieman12341@yahoo.com>
2018-09-18 21:56:27 -0500

    starte

---
+++ lab2/lab2/main.c
@@ -0,0 +1,28 @@
+/*
+lab 2
+Luke Owens
+1001105628
+
+to compile, type:
+
+gcc main.c -o exe
+./exe
+
+*/
+
+/*
+description:
+
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+
+
+
+
+	return 0;
+}
